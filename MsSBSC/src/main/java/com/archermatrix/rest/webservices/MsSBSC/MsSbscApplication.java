35b3819838017c0932bec4e27848fd7896aa51a1
Dev10-Sheldon <sehbailey455@live.com>
2023-04-23 15:48:38 -0400

    initial setup

---
+++ MsSBSC/src/main/java/com/archermatrix/rest/webservices/MsSBSC/MsSbscApplication.java
@@ -0,0 +1,13 @@
+package com.archermatrix.rest.webservices.MsSBSC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSbscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSbscApplication.class, args);
+	}
+
+}
---
+++ MsSBSC/src/test/java/com/archermatrix/rest/webservices/MsSBSC/MsSbscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archermatrix.rest.webservices.MsSBSC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSbscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
